8d27a2ef2afd864d428f8074f3c91270c9afa465
Kushagra <kushagra9nov.com>
2021-05-07 09:57:17 +0530

    shell script updated

---
+++ polybar/launch.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch Polybar
+polybar left -c ~/.config/polybar/config &
+picom &
+xgamma -gamma 0.85
