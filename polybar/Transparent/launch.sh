aa71d9de6da48e9d81d297ad765c543da5dab661
Kushagra Kaushal <kushagra9nov@gmail.com>
2021-05-13 13:25:28 +0530

    updated

--- polybar/Transparent/launch.sh
+++ polybar/Transparent/launch.sh
@@ -7,4 +7,4 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch Polybar
-polybar bar -c ~/.config/pol/config &
+polybar bar -c ~/.config/polybar_transparent/config &
