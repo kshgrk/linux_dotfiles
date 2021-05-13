8900417b12a66171b4866620c743807419a727c4
Kushagra Kaushal <kushagra9nov@gmail.com>
2021-05-13 22:26:57 +0530

    updated

---
+++ rofi/drun.sh
@@ -0,0 +1,2 @@
+#!/bin/env bash
+rofi -modi drun -show drun -line-padding 4 -columns 2 -padding 50 -hide-scrollbar -terminal alacritty -show-icons -drun-icon-theme "Candy-icons" -font "Droid Sans Regular 10"
