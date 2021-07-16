ea581166bff6ece07abfe20c989beade9015570b
kshgrk <kushagra9nov@gmail.com>
2021-07-16 18:17:15 +0530

    changed name

--- polybar/Simple/scripts/checkupdates.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-updates=$(checkupdates)
-
-printf '%s\n' "$updates" > /tmp/bspwm-eos-polybar-updates
-
-count=$(wc -l "/tmp/bspwm-eos-polybar-updates" | awk '{print $1}')
-
-echo "%{T1}%{T-} %{T2}$count%{T-}"
